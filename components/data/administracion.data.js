0b73861e77e783b1608a0fcc50c46660ec60c87d
Puwiii <Sebastian.mathieur@gmail.com>
2021-09-20 23:37:15 -0300

    fix strong inner administracion in homepage

--- components/data/administracion.data.js
+++ components/data/administracion.data.js
@@ -5,7 +5,14 @@ export const administracionData = [
     name: "Barbarich Alejandra Claudia",
     mediaSource: "/images/photos/directora.png",
     description: (
-      <strong>
+      <strong
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px",
+          justifyContent: "center",
+        }}
+      >
         Directora · <BiPhone /> 03455 - 427072{" "}
       </strong>
     ),
@@ -14,7 +21,14 @@ export const administracionData = [
     name: "Rodriguez Siciliano Veronica",
     mediaSource: "/images/photos/secretaria.png",
     description: (
-      <strong>
+      <strong
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px",
+          justifyContent: "center",
+        }}
+      >
         Secretaria técnica · <BiPhone /> 03455 - 427072{" "}
       </strong>
     ),
@@ -23,7 +37,14 @@ export const administracionData = [
     name: "Lifschitz Nieves Lorena",
     mediaSource: "/images/photos/administradora.png",
     description: (
-      <strong>
+      <strong
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "5px",
+          justifyContent: "center",
+        }}
+      >
         Administradora · <BiPhone /> 03455 - 423446{" "}
       </strong>
     ),
